a7715183d1a83d8ad30792c8dec0d7285b4ebeb4
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-08 23:24:47 +0300

    changed port to 5000

--- backend/app.py
+++ backend/app.py
@@ -21,4 +21,4 @@ def home():
     return {"check": "Checking if the route works"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port= 8000, debug=True)
+    app.run(host="0.0.0.0", port= 5000, debug=True)
