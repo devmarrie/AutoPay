b675b6c70003db1e9a0e5085288f3794a61361cd
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-08 08:36:27 +0300

    needs data

---
+++ autopay/src/data/NeedsData.js
@@ -0,0 +1,25 @@
+
+export const needsPresent = [
+   { 
+    need: "Rent",
+    amount: '55,000',
+    duedate: "05/07/2022",
+    reminderdate: "02/07/2022",
+    remove: "remove"
+  },
+  { 
+    need: "Transport",
+    amount: '5,000',
+    duedate: "12/07/2022",
+    reminderdate: "08/07/2022",
+    remove: "remove"
+  },
+  { 
+    need: "Wifi",
+    amount: '10,000',
+    duedate: "03/07/2022",
+    reminderdate: "30/06/2022",
+    remove: "remove"
+  }
+
+]
