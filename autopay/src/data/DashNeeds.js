f66d90c2e719792d273e4f908af423d3c716eee5
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-08 08:36:16 +0300

    dashboard data

---
+++ autopay/src/data/DashNeeds.js
@@ -0,0 +1,33 @@
+import HomeIcon from '@mui/icons-material/Home';
+import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined';
+import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
+import WifiOutlinedIcon from '@mui/icons-material/WifiOutlined';
+import SchoolIcon from '@mui/icons-material/School';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+
+export const dashboardNeed = [
+    {
+        icon: <HomeIcon />,
+        text: "Rent"
+    },
+    {
+        icon: <WaterDropOutlinedIcon />,
+        text: "Water"
+    },
+    {
+        icon: <EmojiObjectsOutlinedIcon />,
+        text: "Lights"
+    },
+    {
+        icon: <WifiOutlinedIcon />,
+        text: "Wifi"
+    },
+    {
+        icon: <SchoolIcon />,
+        text: "Studies"
+    },
+    {
+        icon: <SubscriptionsIcon />,
+        text: "Subscriptions"
+    }
+]
