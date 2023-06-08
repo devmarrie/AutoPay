dc8cba0203deab8680f59276ed82d8711028cbcc
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-08 08:37:22 +0300

    paid needs in pay page

---
+++ autopay/src/data/TableData.js
@@ -0,0 +1,27 @@
+import HomeIcon from '@mui/icons-material/Home';
+import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined';
+import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
+import WifiOutlinedIcon from '@mui/icons-material/WifiOutlined';
+
+export const tableCleared = [
+{
+    bill: HomeIcon,
+    stateb: "Paid",
+    date: "24/07/2023" 
+},
+{
+    bill: WaterDropOutlinedIcon,
+    stateb: "Paid",
+    date: "12/05/2023" 
+},
+{
+    bill: EmojiObjectsOutlinedIcon,
+    stateb: "Paid",
+    date: "04/05/2023" 
+},
+{
+    bill: WifiOutlinedIcon,
+    stateb: "Paid",
+    date: "24/05/2023" 
+}
+]
