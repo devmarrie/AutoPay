765360896a7d44ccb500aecf65334901f845ffc3
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-08 22:31:16 +0300

    to dict function to convertvalues  to a dict

--- backend/models/base_model.py
+++ backend/models/base_model.py
@@ -7,3 +7,6 @@ class BaseModel(db.Model):
     id = db.Column(db.String(60), primary_key=True, default=str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
