2e555d69c8aa332b1c4bc9f08a7e0007ad46a8c0
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-08 23:25:20 +0300

    update and delete need route

--- backend/controllers/routes.py
+++ backend/controllers/routes.py
@@ -22,3 +22,26 @@ def add_need():
 def get_needs():
     needs =  Need.query.all()
     return jsonify([n.to_dict() for n in needs])
+
+@need_routes.route('/update_need/<string:id>', methods=['PUT'])
+def update_need(id):
+    need = Need.query.get(id)
+    if not need:
+        return jsonify({'error': 'Need not found' }), 404
+    data = request.get_json()
+    need.need = data.get('need', need.need)
+    need.amount = data.get('amount', need.amount)
+    need.duedate = data.get('duedate', need.duedate)
+    need.reminderdate = data.get('reminderdate', need.reminderdate)
+    db.session.commit()
+    return jsonify(need.to_dict())
+
+@need_routes.route('/delete_need/<string:id>', methods=['DELETE'])
+def del_need(id):
+    n = Need.query.get(id)
+    if n:
+        db.session. delete(n)
+        db.session.commit()
+        return jsonify({'message': f'Need belonging to {id} deleted successfully'})
+    else:
+        return jsonify({'error': f'Need with {id} not found'}), 404
