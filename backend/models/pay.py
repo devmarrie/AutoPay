acb43bf117b0e59d180d486e9d95544dadddd371
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-09 08:31:48 +0300

    many payments one user

--- backend/models/pay.py
+++ backend/models/pay.py
@@ -5,3 +5,4 @@ class Pay(BaseModel):
     __tablename__ = 'pay'
     amount = db.Column(db.Integer(), nullable=False)
     mpesano = db.Column(db.Integer(), nullable=False)
+    user_id = db.Column(db.String(60), db.ForeignKey('users.id'), nullable=False)
