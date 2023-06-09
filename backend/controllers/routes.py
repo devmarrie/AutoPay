a6f30a275c89309964f59cec55e2b6bbc9fdcd50
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-09 08:29:58 +0300

    excec

--- backend/controllers/routes.py
+++ backend/controllers/routes.py
@@ -1,8 +1,54 @@
 from flask import Blueprint, jsonify, request
 from models.need import Need
+from models.user import User
+from models.pay import Pay
+from models.history import History
 from models.database import db
 
 need_routes = Blueprint('need_routes', __name__)
+users_routes = Blueprint('user_routes', __name__)
+pay_routes = Blueprint('pay_routes', __name__)
+hist_routes = Blueprint('hist_routes', __name__)
+
+"""Users routes"""
+@users_routes.route('/new_user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+    password = data['password']
+    usr = User(name=name, email=email, password=password)
+    db.session.add(usr)
+    db.session.commit()
+    return jsonify({'message': 'User created succesfully'})
+
+@users_routes.route('/get_users')   
+def get_users():
+    users =  User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+"""Pay routes"""
+@pay_routes.route('/new_payment', methods=['POST'])
+def create_payment():
+    data = request.get_json()
+    amount = data['amount']
+    mpesano = data['mpesano']
+    user_id = data['user_id']
+    p = Pay(amount=amount, mpesano=mpesano, user_id=user_id)
+    db.session.add(p)
+    db.session.commit()
+    return jsonify({'message': 'Payment created succesfully'})
+
+"""History routes"""
+@hist_routes.route('/new_history', methods=['POST'])
+def create_history():
+    data = request.get_json()
+    status = data['status']
+    code = data['code']
+    h = History(status=status, code=code)
+    db.session.add(h)
+    db.session.commit()
+    return jsonify({'message': 'History created succesfully'})
 
 """Needs routes"""
 @need_routes.route('/add_need', methods=['POST'])
