b6441b3a284527a311c8322e76eaec559d54de3d
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-10 21:54:34 +0300

    update and delete user routes

--- backend/controllers/usr_routes.py
+++ backend/controllers/usr_routes.py
@@ -20,3 +20,26 @@ def create_user():
 def get_users():
     users =  User.query.all()
     return jsonify([user.to_dict() for user in users])
+
+
+@users_routes.route('/update_user/<string:id>', methods=['PUT'])
+def update_user(id):
+    us = User.query.get(id)
+    if not us:
+        return jsonify({'error': 'User not found' }), 404
+    data = request.get_json()
+    us.name = data.get('name', us.name)
+    us.email = data.get('email', us.email)
+    us.password = data.get('password', us.password)
+    db.session.commit()
+    return jsonify(us.to_dict())
+
+@users_routes.route('/delete_user/<string:id>', methods=['DELETE'])
+def del_need(id):
+    u = User.query.get(id)
+    if u:
+        db.session. delete(u)
+        db.session.commit()
+        return jsonify({'message': f'User belonging to {id} deleted successfully'})
+    else:
+        return jsonify({'error': f'User with {id} not found'}), 404
