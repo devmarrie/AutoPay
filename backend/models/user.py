4975bc6f5d707479a7b5840094f22fd094c44e89
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-12 22:58:24 +0300

    increasing password size

--- backend/models/user.py
+++ backend/models/user.py
@@ -5,6 +5,6 @@ class User(BaseModel):
     __tablename__ = 'users'
     name = db.Column(db.String(60), nullable=False)
     phone_no = db.Column(db.String(60),  unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(260), nullable=False)
     payments = db.relationship('Pay', backref='users')
     needs = db.relationship('Need', backref='users')
