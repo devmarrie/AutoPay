861bf5aece228a6dbb8a4d15ae459d6a0becc697
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-12 13:43:15 +0300

    intergrating africas talking

--- backend/controllers/nd_routes.py
+++ backend/controllers/nd_routes.py
@@ -1,10 +1,31 @@
 from flask import Blueprint, jsonify, request
+from apscheduler.schedulers.background import BackgroundScheduler
 from models.need import Need
+from models.user import User
 from models.database import db
 from datetime import datetime
+import africastalking
+import phonenumbers
 
 need_routes = Blueprint('need_routes', __name__)
 
+# Background scheduler
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+# Africas Talking credentials
+username = 'sandbox'
+api_key = '710e7cbe8961305d6c90cfbf2bdf1abbad728e3405eb75da0c967c9225d73938'
+africastalking.initialize(username, api_key)
+sms = africastalking.SMS
+
+# Function to send sms
+def send_sms(to, message):
+    try:
+        response = sms.send(message, [to])
+        print(response)
+    except Exception as e:
+        print(f'An error occoured while sending message {e}')
 
 """Needs routes"""
 @need_routes.route('/add_need', methods=['POST'])
@@ -16,10 +37,20 @@ def add_need():
     storable_date = duedate.strftime('%Y-%m-%d %H:%M')
     user_id = data['user_id']
     history_id = data['history_id']
+
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
     ned = Need(need=need, amount=amount, duedate=storable_date,
                user_id=user_id, history_id=history_id )
     db.session.add(ned)
     db.session.commit()
+
+    parsed_number = phonenumbers.parse(user.phone_no, None)
+    formatted_no = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
+    send_sms(formatted_no, f'A new need called {need} was created')
+
     return jsonify({'message': 'New need created successfully'})
 
 
