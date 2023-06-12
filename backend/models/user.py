0a8bb3b32eef82ce947140f65e6f876319cfa188
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-12 13:44:09 +0300

    db replacement of email with phone no

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,7 +4,7 @@ from models.database import db
 class User(BaseModel):
     __tablename__ = 'users'
     name = db.Column(db.String(60), nullable=False)
-    email = db.Column(db.String(120),  unique=True, nullable=False)
+    phone_no = db.Column(db.String(60),  unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     payments = db.relationship('Pay', backref='users')
     needs = db.relationship('Need', backref='users')
