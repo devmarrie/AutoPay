ae1c955116db507cc3f9bc9c3d5c759f8296d53c
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-12 09:21:56 +0300

    deleting reminder date from the database

--- backend/models/need.py
+++ backend/models/need.py
@@ -6,7 +6,6 @@ class Need(BaseModel):
     need = db.Column(db.String(50), unique=True, nullable=False)
     amount = db.Column(db.Integer(), nullable=False)
     duedate = db.Column(db.DateTime, nullable=False)
-    reminderdate = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.String(60), db.ForeignKey('users.id'), nullable=False)
     history_id = db.Column(db.String(60), db.ForeignKey('history.id'), nullable=False)
 
