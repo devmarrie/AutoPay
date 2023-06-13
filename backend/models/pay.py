829ebfa23c7569b440d2eee748b533339a54c626
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-13 12:38:12 +0300

    recofiguring the db

--- backend/models/pay.py
+++ backend/models/pay.py
@@ -4,5 +4,7 @@ from models.database import db
 class Pay(BaseModel):
     __tablename__ = 'pay'
     amount = db.Column(db.Integer(), nullable=False)
+    mpesa_receipt_number = db.Column(db.String(100), nullable=False)
+    transaction_date = db.Column(db.DateTime, nullable=False)
     mpesano = db.Column(db.Integer(), nullable=False)
-    user_id = db.Column(db.String(60), db.ForeignKey('users.id'), nullable=False)
+    #user_id = db.Column(db.String(60), db.ForeignKey('users.id'), nullable=False)
