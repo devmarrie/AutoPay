abb1db4ba694a3bbec6cc33d3dfeb11f4cbd753f
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-14 11:57:14 +0300

    cahnging name to username and adding flask user

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,10 @@
 from .base_model import BaseModel
 from models.database import db
+from flask_user import UserMixin
 
-class User(BaseModel):
+class User(BaseModel, UserMixin):
     __tablename__ = 'users'
-    name = db.Column(db.String(60), nullable=False)
+    username = db.Column(db.String(60), nullable=False)
     phone_no = db.Column(db.String(60),  unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
     #payments = db.relationship('Pay', backref='users')
