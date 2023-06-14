7a4d1253a3a63b0f75cc5d7cbefee2c5a4aed0d4
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-14 16:58:32 +0300

    securing my data

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,41 @@
 from flask import Flask
 from models.database import db, init_db
 from flask_migrate import Migrate
+from flask_user import UserManager
+from flask_login import LoginManager
+from dotenv import load_dotenv
+from models.user import User
 from controllers import need_routes, users_routes, pay_routes, hist_routes
+import os
+import secrets
 
 app = Flask(__name__)
 
+load_dotenv()
+# generate secretkey
+secret_key = secrets.token_hex(16)
+
 # Db configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:post_123@localhost:5432/autopay"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE')
+app.config['USER_EMAIL_SENDER_EMAIL'] = os.getenv('EMAIL')
 app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = secret_key
 
 init_db(app)
 migrate = Migrate(app, db)
+
+"""User Intergrations"""
+user_manager = UserManager(app, db, User)
+
+"""user loader"""
+login_manager = LoginManager(app)
+login_manager.login_view = 'user_routes.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(str(user_id))
+
+#csrf = CSRFProtect(app)
 
 # Once connectivity is established create the tables
 with app.app_context():
