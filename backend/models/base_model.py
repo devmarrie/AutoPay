58ba92c19796c9418e3126c15574fbfb04d9aca9
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-16 23:50:08 +0300

    adding the lambda functionality

--- backend/models/base_model.py
+++ backend/models/base_model.py
@@ -4,7 +4,7 @@ from models.database import db
 
 class BaseModel(db.Model):
     __abstract__ = True
-    id = db.Column(db.String(60), primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(db.String(60), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
