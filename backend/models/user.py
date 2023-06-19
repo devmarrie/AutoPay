d035ce452c645ce45fa2313857ad6571ae24d9a9
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-19 21:00:50 +0300

    redefining the db

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,9 +4,14 @@ from flask_user import UserMixin
 
 class User(BaseModel, UserMixin):
     __tablename__ = 'users'
-    username = db.Column(db.String(60), nullable=False)
-    phone_no = db.Column(db.String(60),  unique=True, nullable=False)
-    password = db.Column(db.String(260), nullable=False)
+    google_id  = db.Column(db.String(260), nullable=False)
+    name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(260), nullable=False)
+    avatar_url = db.Column(db.String(260), nullable=False)
+    #phone_no = db.Column(db.String(60),  unique=True, nullable=False)
     #payments = db.relationship('Pay', backref='users')
     needs = db.relationship('Need', backref='users')
     histories = db.relationship('History', backref='users')
+
+    def get_id(self):
+        return str(self.id)
